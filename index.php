9bfded9cd8d3bdbf76d3d2c43a89ff5427e67c62
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2023-06-27 17:12:10 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
     </head>
     <body>
         <h1>hello world</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque totam reiciendis optio necessitatibus sapiente illo, explicabo provident incidunt pariatur cumque, commodi nisi, deserunt perferendis! In quod ullam expedita minus tempora.</p>
     </body>
 </html>
