19dcc6617ff1e318dfe21d257eac07450be8b863
Ikhsan-Fatahillah <ikhsanngkalu@gmail.com>
2023-06-27 17:17:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -5,5 +5,9 @@
     <body>
         <h1>hello world</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque totam reiciendis optio necessitatibus sapiente illo, explicabo provident incidunt pariatur cumque, commodi nisi, deserunt perferendis! In quod ullam expedita minus tempora.</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
     </body>
 </html>
